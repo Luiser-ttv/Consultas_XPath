989e29462e7a2e0c2efaa7d6f32b0637ab1f5368
rioja <>
2020-11-27 00:46:42 +0100

    Completadas las consultas de XPath.

--- src/Gestionar_XPath.java
+++ src/Gestionar_XPath.java
@@ -7,6 +7,7 @@ import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathFactory;
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 /*
@@ -46,6 +47,30 @@ public class Gestionar_XPath {
             return -1;
         }
     }
+    
+    private String[] procesarLibro(Node n) {
+        String datos[] = new String[3];
+        Node ntemp = null;
+        int contador = 1;
+
+        datos[0] = n.getAttributes().item(0).getNodeValue();
+
+        NodeList nodos = n.getChildNodes();
+
+        for (int i = 0; i < nodos.getLength(); i++) {
+
+            ntemp = nodos.item(i);
+
+            if (ntemp.getNodeType() == Node.ELEMENT_NODE) {
+                //datos[contador] = ntemp.getChildNodes().item(0).getNodeValue();
+                datos[contador] = ntemp.getFirstChild().getNodeValue();
+                contador++;
+            }
+        }
+        return datos;
+    }
+
+    
 
    public String Ejecutar_XPath(String consulta) {
        
@@ -58,8 +83,21 @@ public class Gestionar_XPath {
            Object result = exp.evaluate(doc, XPathConstants.NODESET);
            NodeList listaNodos = (NodeList) result;
            
+           
            for(int i = 0; i<listaNodos.getLength(); i++){
-               salida = salida + "\n" + listaNodos.item(i).getFirstChild().getNodeValue();
+               //salida = salida + "\n" + listaNodos.item(i).getFirstChild().getNodeValue();
+               if(listaNodos.item(i).getNodeName().equals("Libro")){
+               
+                  String datos_nodo[] = procesarLibro(listaNodos.item(i));
+                salida = salida + "\n " + "Publicado en: " + datos_nodo[0];
+                salida = salida + "\n " + "El titulo es: " + datos_nodo[1];
+                salida = salida + "\n " + "El autor es: " + datos_nodo[2];
+                salida = salida + "\n ---------------------";
+                
+               }else{
+                   salida = salida + "\n" + listaNodos.item(i).getFirstChild().getNodeValue();
+               }
+               
            }
            
             return salida;
